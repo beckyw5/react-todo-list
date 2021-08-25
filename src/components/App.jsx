be35b5f76cb2e34380837fee29e40d4542a89f1e
Becky Wilson <rebecca.wilson2@sky.uk>
2021-08-25 20:54:00 +0100

    working on adding to the array using a spread operator, with hardcoded values

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,11 +21,20 @@ function App() {
         }
     ]);
 
+    function addTodo() {
+        setTodos([...todos, {
+            id: 4,
+            title: 'Testing',
+            isComplete: false,
+        },
+        ]);
+    }
+
     return (
         <div className="todo-app-container">
             <div className="todo-app">
                 <h2>Todo App</h2>
-                <form action="#">
+                <form action="#" onSubmit={addTodo}>
                     <input
                         type="text"
                         className="todo-input"
